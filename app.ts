5b1172dc5b29774b4f86e61bb2edc935ccecd332
kobabasu <keiji.kobayashi.web@gmail.com>
2015-06-14 09:04:47 +0900

    first commit

---
+++ app.ts
@@ -0,0 +1,10 @@
+/// <reference path='../vendor/typings/jquery/jquery.d.ts' />
+
+import $ = require('jquery');
+import hello = require('./hello');
+// import rest = require('./rest');
+
+$(document).ready(function() {
+  new hello.Hello;
+  // new rest.Rest;
+});
---
+++ rest.ts
@@ -0,0 +1,26 @@
+/// <reference path='../vendor/typings/jquery/jquery.d.ts' />
+
+import $ = require('jquery');
+
+export class Rest {
+  host: string = 'http://localhost:8080/api/'
+
+  constructor() {
+    this.getIndex();
+  }
+
+  private getIndex(): void {
+    $.ajax({
+      url: this.host + 'users',
+      type: 'GET',
+      error: function() {
+        console.log('error');
+      },
+      success: function(json) {
+        $.each(JSON.parse(json), function(i, item) {
+          $('body').prepend(item.name + '<br>');
+        });
+      }
+    });
+  }
+}
